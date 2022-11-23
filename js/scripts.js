5e0f13ec2d26fcbdba95c97c642969944c86aa25
Olexie <116206464+Olexie@users.noreply.github.com>
2022-11-23 13:31:39 +0000

    replace the code in scripts for the pokemonList
    If comimted, will replace the initial code in Js.scripts to the array of pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favouriteFood = 'chocolate';
-document.write(favouriteFood);
+let pokemonList = [
+    {name:'Charmander', height: 2, category: 'lizard', type:'fire'},
+    {name: 'Squirtle', height: 1.8, category:'turtle', type:'water'},
+    {name: 'Zubat', height: 2.7, category:'bat', type:['poison','fly']},
+    {name:'Vulpix', height: 2, category:'fox', type:'fire'}
+];
