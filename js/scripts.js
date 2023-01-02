8ca2206424f123aaa747c29fa43a1a6cabf8acf3
Olexie <116206464+Olexie@users.noreply.github.com>
2023-01-02 18:59:04 +0200

    adding forEach function
    If commited will substitute for function to forEach function to iterate over pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,25 @@ let pokemonList = [
 
 //iterating through objects in array creatong a loop
 
-for (let i= 0; i< pokemonList.length; i++) {
-    if (pokemonList[i].height >2 ) {
-        document.write(pokemonList[i].name + pokemonList[i].height + ' meters tall' + ' You caught the big one!' + '<br>')
+pokemonList.forEach (function(pokemon) {
+    if (pokemon.height >2 ) {
+        document.write(pokemon.name + ' '+ pokemon.height + ' meters tall' + ' You caught the big one!' + '<br>')
 
-    } else if (pokemonList[i].height > 1 && pokemonList[i].height < 2.1) {
-        document.write(pokemonList[i].name +  pokemonList[i].height + ' meters tall' + '<br>')
+    } else if (pokemon.height > 1 && pokemon.height < 2.1) {
+        document.write(pokemon.name + ' '+ pokemon.height + ' meters tall' + '<br>')
     } else {
-        document.write (pokemonList[i].name +  pokemonList[i].height + ' meters tall' + 'that is small pokemon' + '<br>')
+        document.write (pokemon.name + ' '+ pokemon.height + ' meters tall' + 'that is small pokemon' + '<br>')
     }  
+});
+
+/*for (let {height, name} of pokemonList) {
+if (height>2) {
+    document.write ('<p class="large-pokemon"> ${name} ${height} meters tall. You caught the big one! </p>')
+} else if (height>1 && height < 2.1) {
+    document.write ('<p class="medium-pokemon"> ${name} ${height} meters tall </p>')
 }
+ else {
+    document.write ('<p class="small-pokemon"> ${name} ${height} meters tall. That is the small one </p>')
+
+}
+}*/
